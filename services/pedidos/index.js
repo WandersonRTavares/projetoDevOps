715d869e828feaadea67daf7e8f8b337b3c5cb41
Palito <95984498+WandersonRTavares@users.noreply.github.com>
2026-03-02 15:44:34 -0300

    alteração do deploymente

--- services/pedidos/index.js
+++ services/pedidos/index.js
@@ -8,3 +8,6 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Pedidos rodando na porta 3000');
 });
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'UP' });
+});
