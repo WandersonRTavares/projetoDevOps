1a455729a66bcbd1eea3b6746c653db63d9a908b
Palito <95984498+WandersonRTavares@users.noreply.github.com>
2026-03-03 00:17:13 -0300

    criação node model

--- services/pedidos/index.js
+++ services/pedidos/index.js
@@ -11,3 +11,23 @@ app.listen(3000, () => {
 app.get('/health', (req, res) => {
     res.status(200).json({ status: 'UP' });
 });
+
+const client = require('prom-client');
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+const httpRequestCounter = new client.Counter({
+    name: 'http_requests_total',
+    help: 'Total de requisições HTTP'
+});
+
+app.use((req, res, next) => {
+    httpRequestCounter.inc();
+    next();
+});
+
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
+});
